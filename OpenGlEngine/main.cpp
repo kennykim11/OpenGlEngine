70a7d6ece4eae4a3a62f8fa288be38f3bbc50b32
Kenny Kim <kennykim11@gmail.com>
2020-10-18 03:16:18 -0400

    shape struct

--- OpenGlEngine/main.cpp
+++ OpenGlEngine/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <vector>
 #define GLEW_STATIC
 #include <GL/glew.h> //GL Extenton Wrangler provides runtime support for determining extentions to use on target platform
 #include <GLFW/glfw3.h> //GLFW is the library for using OpenGL in a windowed context
+
+struct shape {
+	std::vector<GLfloat> vertices;
+	GLuint VAO;
+	GLuint VBO;
+};
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
@@ -44,23 +51,23 @@ int main()
 
 
 	// === OBJECTS ===
-	GLfloat vertices[] =
-	{
-		-0.5f, -0.5f, 0.0f,
-		0.5, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+	shape triangle;
+	triangle.vertices = {
+		-1.0f, -0.5f, 0.0f,
+		0.0f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f,
 	};
 	GLuint VBO, VAO; 
 	// =Vertex Buffer Objects contain only the data for vertices=
 	// =Vertex Array Objects contain details and format for those vertices=
 	//(howManyElementsInArray, addressOfArray)
-	glGenVertexArrays(1, &VAO); //creating vertex arrays
-	glGenBuffers(1, &VBO); //creating vertex buffer
-	glBindVertexArray(VAO); //activate the array
-	glBindBuffer(GL_ARRAY_BUFFER, VBO); //sets the Array Buffer to VBO
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //describes properies of the VBO
+	glGenVertexArrays(1, &triangle.VAO); //creating vertex array
+	glGenBuffers(1, &triangle.VBO); //creating vertex buffer
+	glBindVertexArray(triangle.VAO); //set current VertexArray to VAO
+	glBindBuffer(GL_ARRAY_BUFFER, triangle.VBO); //sets the current ArrayBuffer to VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*triangle.vertices.size(), &triangle.vertices[0], GL_STATIC_DRAW); //describes data properies of the VBO
 	//(index, numOfElementsInArray, typeOfElements, normalized aka [0, 1], sizeOfArray, offset??)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0); //defines an array of generic vertex attribute data for index 0
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0); //defines an array of vertex attribute data for index 0
 	glEnableVertexAttribArray(0); //enables the VertexAttribArray with the index 0
 	glBindBuffer(GL_ARRAY_BUFFER, 0); //Unbinds the array buffer (0 means unbind)
 	glBindVertexArray(0); //unbind vertex array object
@@ -75,7 +82,7 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT); //clearing the buffer
 
 		// === DRAWING ===
-		glBindVertexArray(VAO);
+		glBindVertexArray(triangle.VAO);
 		//(mode, firstIndex, count)
 		glDrawArrays(GL_TRIANGLES, 0, 3); 
 		glBindVertexArray(0); //unbind
