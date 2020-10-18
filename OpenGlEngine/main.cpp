f5306e8a1301306edc037e7458606a63e37b5d78
Kenny Kim <kennykim11@gmail.com>
2020-10-18 02:19:51 -0400

    2: Draw a triangle

--- OpenGlEngine/main.cpp
+++ OpenGlEngine/main.cpp
@@ -8,6 +8,7 @@ const GLint WIDTH = 800, HEIGHT = 600;
 
 int main()
 {
+	// === SETUP ===
 	glfwInit(); //Initialize
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //Set to version 3.3
@@ -17,11 +18,9 @@ int main()
 
 	//(width, height, windowTitle, GLFWmonitor for fullscreen, GLFWWindow to use context of existing window)
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Opening a window", nullptr, nullptr);
-
 	//Getting the actual window size, useful for accomidating high-density screens
 	int screenWidth, screenHeight;
 	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
-
 	// Check if the window creation was successful by checking if the window object is a null pointer or not
 	if (window == nullptr)
 	{
@@ -32,26 +31,42 @@ int main()
 	}
 
 	glfwMakeContextCurrent(window); //setting our new window to be the current context
-
-	// This allows GLEW take the modern approach to retrive function pointers and extensions
-	glewExperimental = GL_TRUE;
-
-	// Initialize GLEW to setup OpenGL function pointers
+	glewExperimental = GL_TRUE; //use modern magic for glew
 	if (GLEW_OK != glewInit())
 	{
-		// If the initalization is not successful, print out the message and exit the program with return value EXIT_FAILURE
 		std::cout << "Failed to initialize GLEW" << std::endl;
 		return EXIT_FAILURE;
 	}
-
 	//setting up viewport
 	//(top, left, width, height)
 	glViewport(0, 0, screenWidth, screenHeight);
 
 
 
+	// === OBJECTS ===
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		0.5, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+	GLuint VBO, VAO; 
+	// =Vertex Buffer Objects contain only the data for vertices=
+	// =Vertex Array Objects contain details and format for those vertices=
+	//(howManyElementsInArray, addressOfArray)
+	glGenVertexArrays(1, &VAO); //creating vertex arrays
+	glGenBuffers(1, &VBO); //creating vertex buffer
+	glBindVertexArray(VAO); //activate the array
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); //sets the Array Buffer to VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //describes properies of the VBO
+	//(index, numOfElementsInArray, typeOfElements, normalized aka [0, 1], sizeOfArray, offset??)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0); //defines an array of generic vertex attribute data for index 0
+	glEnableVertexAttribArray(0); //enables the VertexAttribArray with the index 0
+	glBindBuffer(GL_ARRAY_BUFFER, 0); //Unbinds the array buffer (0 means unbind)
+	glBindVertexArray(0); //unbind vertex array object
 
-	//Game Loop! Runs until window is closed
+
+	// === GAME LOOP ===
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents(); //check for events
@@ -59,12 +74,18 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); //specifies the RGBA values which will be used by glClear to clear the color buffer
 		glClear(GL_COLOR_BUFFER_BIT); //clearing the buffer
 
+		// === DRAWING ===
+		glBindVertexArray(VAO);
+		//(mode, firstIndex, count)
+		glDrawArrays(GL_TRIANGLES, 0, 3); 
+		glBindVertexArray(0); //unbind
 
 		glfwSwapBuffers(window); //swap buffers at the very end after finished rendering
 	}
 
 
-
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 	glfwTerminate();
 	return EXIT_SUCCESS;
 }
